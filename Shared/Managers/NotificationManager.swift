541507edacc2786eee2872383cdec359b2f7b202
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2020-11-29 22:50:25 -0600

    Logic for prepping loop data for notification manager

--- Shared/Managers/NotificationManager.swift
+++ Shared/Managers/NotificationManager.swift
@@ -15,13 +15,14 @@ struct NotificationManager {
     static let shared = NotificationManager()
     
     private struct NotificationData {
-        
+        let title: String
+        var time: Date
+        let threadIdentifier: String
     }
     
     func refreshNotifications() {
         guard let loops = fetchCoreDataLoops() else { return }
         let notifications = createNotificationData(from: loops)
-        // Create notification array
     }
     
     private func createNotificationData(from loops: [CoreDataLoop]) -> [NotificationData] {
@@ -29,7 +30,36 @@ struct NotificationManager {
         
         for loop in loops {
             if loop.isEnabled {
+                // Makes sure that startTime and endTime are non-nil
+                guard var startTime = loop.startTime,
+                      var endTime = loop.endTime else { continue }
                 
+                // Set times to current date
+                let startTimeComponents = Calendar.current.dateComponents([.hour, .minute], from: startTime)
+                startTime = Calendar.current.date(byAdding: startTimeComponents, to: Date())!
+                
+                let endTimeComponenets = Calendar.current.dateComponents([.hour, .minute], from: endTime)
+                endTime = Calendar.current.date(byAdding: endTimeComponenets, to: Date())!
+                
+                // If end time is prior to start time, move ahead a day (past midnight)
+                if endTime < startTime {
+                    endTime = Calendar.current.date(byAdding: .day, value: 1, to: endTime)!
+                }
+                
+                // Create notification to work with
+                var notification = NotificationData(
+                    title: loop.name ?? "Your timer has looped!",
+                    time: startTime,
+                    threadIdentifier: loop.id!.uuidString
+                )
+                
+                while startTime < endTime {
+                    notification.time = startTime
+                    notifications.append(notification)
+                    
+                    // Add the interval to the start time
+                    startTime = Calendar.current.date(byAdding: .minute, value: Int(loop.interval * 60), to: startTime)!
+                }
             }
         }
         
