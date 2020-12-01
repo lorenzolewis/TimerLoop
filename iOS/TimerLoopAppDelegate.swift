8d92eafc16e6af21c35a9e4263311108e6583b89
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2020-12-01 09:41:13 -0600

    Fix issue where notifications wouldn't display when app was in foreground

--- iOS/TimerLoopAppDelegate.swift
+++ iOS/TimerLoopAppDelegate.swift
@@ -7,9 +7,20 @@
 
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
     // Allows notifications to be received when the app is open
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        log.debug("Notification received while in foreground.")
+        
         completionHandler([.banner, .sound])
     }
 }
